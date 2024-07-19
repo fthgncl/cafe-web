a87b050a903a73ac466f7dff2d6d1bcb77123658
fthgncl <gencal.fatih61@gmail.com>
2024-07-19 17:23:42 +0300

    Web Socket eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
+  const [message, setMessage] = useState('');
 
-    </div>
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+
+    ws.onmessage = (event) => {
+      setMessage(event.data);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+      <div className="App">
+        <h1>WebSocket Test</h1>
+        <p>Message from server: {message}</p>
+      </div>
   );
 }
 
